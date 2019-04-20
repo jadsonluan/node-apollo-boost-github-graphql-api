643d2f1b81ae8b2e20f78e6c87050103ba15eb8a
Jadson Luan <jadsonluan1@gmail.com>
2019-04-20 15:45:05 -0300

    Implement pagiantion, variables, nested objects and list fields

--- src/index.js
+++ src/index.js
@@ -13,17 +13,28 @@ const client = new ApolloClient({
   }
 });
 
-const GET_ORGANIZATION = gql`
-  {
-    organization(login: "the-road-to-learn-react") {
+const GET_REPOSITORIES_OF_ORGANIZATION = gql`
+  query($organization: String!) {
+    organization(login: $organization) {
       name
       url
+      repositories(first: 5) {
+        edges {
+          node {
+            name
+            url
+          }
+        }
+      }
     }
   }
 `;
 
 client
   .query({
-    query: GET_ORGANIZATION,
+    query: GET_REPOSITORIES_OF_ORGANIZATION ,
+    variables: {
+      organization: 'the-road-to-learn-react',
+    }
   })
   .then(console.log)
