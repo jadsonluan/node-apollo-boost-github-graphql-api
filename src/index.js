183cfcc3b6cba3b2b0697763155e1ed53b7c1b3a
Jadson Luan <jadsonluan1@gmail.com>
2019-04-20 16:09:01 -0300

    Implement pagination exercises and add/remove star mutation

--- src/index.js
+++ src/index.js
@@ -14,27 +14,106 @@ const client = new ApolloClient({
 });
 
 const GET_REPOSITORIES_OF_ORGANIZATION = gql`
-  query($organization: String!) {
+  query ($organization: String!, $cursor: String) {
     organization(login: $organization) {
       name
       url
-      repositories(first: 5) {
-        edges {
-          node {
-            name
-            url
-          }
+      repositories(
+        first: 5, 
+        orderBy: {field: STARGAZERS, direction: DESC}, 
+        after: $cursor
+      ) {
+        edges { node {...repository } }
+        pageInfo {
+          hasNextPage
+          endCursor
         }
+      }
+    }
+  }
+
+  fragment repository on Repository {
+    name
+    url
+  }
+
+`;
+
+const ADD_STAR = gql`
+  mutation AddStar($repositoryId: ID!) {
+    addStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
+      }
+    }
+  }
+`;
+
+const REMOVE_STAR = gql`
+  mutation RemoveStar($repositoryId: ID!) {
+    removeStar(input: { starrableId: $repositoryId }) {
+      starrable {
+        id
+        viewerHasStarred
       }
     }
   }
 `;
 
 client
+  // first query
   .query({
     query: GET_REPOSITORIES_OF_ORGANIZATION ,
     variables: {
       organization: 'the-road-to-learn-react',
     }
   })
-  .then(console.log)
+  // resolve first query
+  .then(response => {
+    const { pageInfo, edges } = response.data.organization.repositories;
+    const { endCursor } = pageInfo;
+
+    console.log('second page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  })
+  // second query
+  .then(({ hasNextPage, endCursor}) => {
+    if (!hasNextPage) throw Error('no next page');
+    return client.query({
+      query: GET_REPOSITORIES_OF_ORGANIZATION,
+      variables: {
+        organization: 'the-road-to-learn-react',
+        cursor: endCursor
+      }
+    })
+  })
+  .then(response => {
+    const { pageInfo, edges } = response.data.organization.repositories;
+    const { endCursor } = pageInfo;
+
+    console.log('second page', edges.length);
+    console.log('endCursor', endCursor);
+
+    return pageInfo;
+  }).catch(console.log);
+
+client
+  .mutate({
+    mutation: ADD_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    },
+  })
+  .then(console.log);
+
+client
+  .mutate({
+    mutation: REMOVE_STAR,
+    variables: {
+      repositoryId: 'MDEwOlJlcG9zaXRvcnk2MzM1MjkwNw==',
+    },
+  })
+  .then(console.log);
